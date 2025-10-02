7aa01da5d59d0815655f7645d8de94ae7c59482e
Rich Olson <rich@zenzig.com>
2025-10-01 20:53:12 -0700

    feat: add cross-platform Socket.IO bridge

--- example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
+++ example-app/android/app/src/main/java/com/example/plugin/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.plugin;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.zenzig.plugins.socketio.CapacitorSocketIOPlugin;
+
+public class MainActivity extends BridgeActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		registerPlugin(CapacitorSocketIOPlugin.class);
+		super.onCreate(savedInstanceState);
+	}
+}
