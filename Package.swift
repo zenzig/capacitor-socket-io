24003343fbe74ec391337ccfb21c4db3ef17104e
Rich Olson <rich@zenzig.com>
2026-01-09 16:40:30 -0800

    chore: upgrade to Capacitor 8.x SPM dependencies

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
             targets: ["CapacitorSocketIOPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-    .package(url: "https://github.com/socketio/socket.io-client-swift", from: "16.1.1")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", from: "16.1.1"),
     ],
     targets: [
         .target(
@@ -19,12 +19,12 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "SocketIO", package: "socket.io-client-swift")
+                .product(name: "SocketIO", package: "socket.io-client-swift"),
             ],
             path: "ios/Sources/CapacitorSocketIOPlugin"),
         .testTarget(
             name: "CapacitorSocketIOPluginTests",
             dependencies: ["CapacitorSocketIOPlugin"],
-            path: "ios/Tests/CapacitorSocketIOPluginTests")
+            path: "ios/Tests/CapacitorSocketIOPluginTests"),
     ]
 )
